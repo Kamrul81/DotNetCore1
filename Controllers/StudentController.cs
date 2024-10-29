31ccd59b2df3c2757440cbd3614340312aa82f0a
Kamrul <1703081kamrul@gmai.com>
2024-10-29 11:51:29 +0600

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,99 @@
+﻿using DotNetCore1.Data;
+using DotNetCore1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentDAL studentDAL = null;
+
+        public StudentController()
+        {
+            studentDAL = new StudentDAL();
+        }
+
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            IEnumerable<Student> students = studentDAL.GetAllStudent();
+            return View(students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            Student student = studentDAL.GetStudentData(id);
+            return View(student);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                studentDAL.AddStudent(student);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Student student = studentDAL.GetStudentData(id);
+            return View(student);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student)
+        {
+            try
+            {
+                studentDAL.UpdateStudent(student);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Student student = studentDAL.GetStudentData(id);
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Student student)
+        {
+            try
+            {
+                studentDAL.DeleteStudent(student.Id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Utility/ConnectionString.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetCore1.Utility
+{
+    public static class ConnectionString
+    {
+        private static string cName = "Server=LAP-0524-0115\\SQLEXPRESS;Database=DbCURDDotNet1;User Id=sa;Password=abc123;Trusted_Connection=True;";
+        public static string CName { get => cName; }
+    }
+}
