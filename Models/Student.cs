66e557b383d939e7ed85ee385c49c32548853344
Kamrul <1703081kamrul@gmai.com>
2024-10-30 16:44:50 +0600

    front and back validation

--- Models/Student.cs
+++ Models/Student.cs
@@ -13,6 +13,10 @@ namespace DotNetCore1.Models
         [Required]
         public string Email { get; set; }
         [Required]
+        [RegularExpression(@"^(017|015|013|014|016|018|019)\d{8}$", ErrorMessage = "Mobile number must start with 017, 015, 013, 014, 016, 018, or 019 and be exactly 11 digits long.")]
+
+        //[RegularExpression(@"^\d{11}$", ErrorMessage = "Mobile number must be exactly 11 digits.")]
+
         public string Mobile { get; set; }
         public string Address { get; set; }
 
